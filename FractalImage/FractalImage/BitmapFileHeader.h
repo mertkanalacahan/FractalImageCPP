04a89ff96ad6c628208e2c008b10ed72df5d161e
mertkan.alacahan <mertkan.alacahan@simsoft.com.tr>
2017-01-06 16:38:34 +0200

    BitmapFileHeader class added

---
+++ FractalImage/FractalImage/BitmapFileHeader.h
@@ -0,0 +1,13 @@
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+
+#pragma pack(push,2)
+struct BitmapFileHeader {
+	char header[2]{'B','M'};
+	int32_t fileSize;
+	int32_t reserved{ 0 };
+	int32_t dataOffset;
+};
+#pragma pack(pop)
+
+#endif
