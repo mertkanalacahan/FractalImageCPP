0bf11c4445fe1b546fae805587c70546b502f4ca
mertkan.alacahan <mertkan.alacahan@simsoft.com.tr>
2017-01-07 13:05:15 +0200

    Bitmap class beginning

--- FractalImage/FractalImage/BitmapFileHeader.h
+++ FractalImage/FractalImage/BitmapFileHeader.h
@@ -2,12 +2,17 @@
 #define BITMAPFILEHEADER_H_
 
 #pragma pack(push,2)
-struct BitmapFileHeader {
-	char header[2]{'B','M'};
-	int32_t fileSize;
-	int32_t reserved{ 0 };
-	int32_t dataOffset;
-};
+
+namespace TryharderMedia {
+
+	struct BitmapFileHeader {
+		char header[2]{ 'B','M' };
+		int32_t fileSize;
+		int32_t reserved{ 0 };
+		int32_t dataOffset;
+	};
+
+}
 #pragma pack(pop)
 
 #endif
---
+++ FractalImage/FractalImage/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World!" << endl;
+
+	system("pause");
+	return 0;
+}
