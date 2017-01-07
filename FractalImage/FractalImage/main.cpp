c0e89823d130b5aff66ba4aed8e4fd2eb41c9c13
mertkan.alacahan <mertkan.alacahan@simsoft.com.tr>
2017-01-07 16:34:57 +0200

    Width, Height scaled to -1,1

--- FractalImage/FractalImage/main.cpp
+++ FractalImage/FractalImage/main.cpp
@@ -10,13 +10,25 @@ int main()
 
 	Bitmap bitmap(WIDTH, HEIGHT);
 
+	double xFractal = 0;
+	double yFractal = 0;
+	double min = 999999;
+	double max = -999999;
+
 	for (int y = 0; y < HEIGHT; y++)
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
-			bitmap.setPixel(x, y, 255, 0, 0);
+			//bitmap.setPixel(x, y, 255, 0, 0);
+			xFractal = (x - WIDTH/2) / (WIDTH/2.0);
+			yFractal = (y - HEIGHT / 2) / (HEIGHT / 2.0);
+
+			if (xFractal < min) min = xFractal;
+			if (xFractal > max) max = xFractal;
 		}
 	}
+
+	cout << min << ", " << max << endl;
 
 	bitmap.write("test.bmp");
 
